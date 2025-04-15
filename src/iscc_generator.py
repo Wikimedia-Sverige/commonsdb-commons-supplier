3cc7ee4db678c6766e5097a40454c659933b809f
Sebastian Berlin <sebastian.berlin@wikimedia.se>
2025-04-15 13:19:59 +0200

    Add logging
    Format includes timestamp and uses semicolon to separate fields. Log level is
    debug.
    
    Added logging before and after ISCC generation that can be used to figure out
    how that takes.

--- src/iscc_generator.py
+++ src/iscc_generator.py
@@ -1,14 +1,25 @@
+import logging
 import sys
 
 import iscc_sdk
 
+logger = logging.getLogger(__name__)
+
 
 class IsccGenerator:
     def generate(self, image_path):
+        logger.info(f"Generating ISCC from image: '{image_path}'.")
         iscc_meta = iscc_sdk.code_iscc(image_path)
+        logger.debug("ISCC generation done.")
         return iscc_meta.iscc
 
 
 if __name__ == "__main__":
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format="{asctime};{name};{levelname};{message}",
+        style="{"
+    )
+
     generator = IsccGenerator()
     print(generator.generate(sys.argv[1]))
