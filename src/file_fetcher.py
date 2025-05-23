889931dcd8bdf85731fd0c789d779a133d59ff48
Sebastian Berlin <sebastian.berlin@wikimedia.se>
2025-05-23 13:57:37 +0200

    Make sure image file isn't removed too early
    Having the temporary directory in the function meant that it was cleaned up when `IsccGenerator` needed it.

--- src/file_fetcher.py
+++ src/file_fetcher.py
@@ -7,10 +7,14 @@ logger = logging.getLogger(__name__)
 
 
 class FileFetcher:
+    def __init__(self):
+        # Create the temporary directory here to make sure it stays around
+        # for as long as it's needed.
+        self._storage = TemporaryDirectory()
+
     def fetch_file(self, page: FilePage) -> str:
         filename = page.title(with_ns=False)
-        storage = TemporaryDirectory()
-        path = f"{storage.name}/{filename}"
+        path = f"{self._storage.name}/{filename}"
         logger.info(f"Downloading file: '{filename}'")
         page.download(path)
         return path
