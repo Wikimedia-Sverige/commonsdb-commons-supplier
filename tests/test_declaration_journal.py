da9cdba97dba135c2fd854fd7e8b6a2499e3e3a9
Sebastian Berlin <sebastian.berlin@wikimedia.se>
2025-06-17 16:34:57 +0200

    Update declaration if it matches page id
    Instead of adding a new declaration update the one already in the journal.
    This is relevant for declarations that not yet have ISCC.

--- tests/test_declaration_journal.py
+++ tests/test_declaration_journal.py
@@ -10,34 +10,40 @@ class DeclarationJournalTestCase(TestCase):
     def test_add_declaration(self):
         self._declaration_journal.add_declaration(
             page_id=123,
-            revision_id=456
+            revision_id=456,
+            image_hash="hash123456789"
         )
         self._declaration_journal.add_declaration(
             page_id=234,
-            revision_id=567
+            revision_id=567,
+            image_hash="hash234567890"
         )
 
         declarations = self._declaration_journal.get_declarations()
 
         assert declarations[0].page_id == 123
         assert declarations[0].revision_id == 456
+        assert declarations[0].image_hash == "hash123456789"
         assert declarations[1].page_id == 234
         assert declarations[1].revision_id == 567
+        assert declarations[1].image_hash == "hash234567890"
 
     def test_update_declaration(self):
-        declaration_id = self._declaration_journal.add_declaration(
+        declaration = self._declaration_journal.add_declaration(
             page_id=123,
-            revision_id=456
+            revision_id=456,
+            image_hash="hash123456789"
         )
         self._declaration_journal.add_declaration(
             page_id=234,
-            revision_id=567
-        )
-        self._declaration_journal.update_declaration(
-            declaration_id,
-            revision_id=678
+            revision_id=567,
+            image_hash="hash234567890"
         )
 
+        self._declaration_journal.update_declaration(
+            declaration,
+            revision_id=678
+        )
         declarations = self._declaration_journal.get_declarations()
 
         assert declarations[0].page_id == 123
@@ -46,25 +52,45 @@ class DeclarationJournalTestCase(TestCase):
         assert declarations[1].revision_id == 567
 
     def test_get_image_hash_match(self):
-        declaration_id = self._declaration_journal.add_declaration(
+        declaration = self._declaration_journal.add_declaration(
             page_id=123,
-            revision_id=456
-        )
-        self._declaration_journal.update_declaration(
-            declaration_id,
+            revision_id=456,
             image_hash="hash123456789"
         )
 
         match = self._declaration_journal.get_image_hash_match("hash123456789")
 
-        assert match == 123
+        assert match == declaration
 
     def test_get_image_hash_match_no_match(self):
         self._declaration_journal.add_declaration(
             page_id=123,
-            revision_id=456
+            revision_id=456,
+            image_hash="hash123456789"
         )
 
         match = self._declaration_journal.get_image_hash_match("hashOTHER")
+
+        assert match is None
+
+    def test_get_page_id_match(self):
+        declaration = self._declaration_journal.add_declaration(
+            page_id=123,
+            revision_id=456,
+            image_hash="hash123456789"
+        )
+
+        match = self._declaration_journal.get_page_id_match(123)
+
+        assert match == declaration
+
+    def test_get_page_id_match_no_match(self):
+        self._declaration_journal.add_declaration(
+            page_id=123,
+            revision_id=456,
+            image_hash="hash123456789"
+        )
+
+        match = self._declaration_journal.get_page_id_match(234)
 
         assert match is None
