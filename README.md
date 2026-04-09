65faa16f925eab97d6337d3aabab99b8531c645b
Sebastian Berlin <sebastian.berlin@wikimedia.se>
2026-04-09 17:04:08 +0200

    Version 0.1.12

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # commonsdb-commons-supplier
 
-Version: 0.1.11
+Version: 0.1.12
 
 ## Requirements
 
--- src/declaration_api_connector.py
+++ src/declaration_api_connector.py
@@ -103,7 +103,7 @@ class DeclarationApiConnector:
                 self._get_tsa(cdbSignature, "commons-db-tsa")
         }
         headers = {
-            "User-Agent": "commonsdb-commons-supplier/0.1.10",
+            "User-Agent": "commonsdb-commons-supplier/0.1.12",
             "Authorization": f"Bearer {self._api_key}",
         }
         logger.info(f"Sending request to '{self._api_endpoint}'.")
