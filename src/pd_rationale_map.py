be5eade7a82373e2eed66ccc5d98060448ff4a3b
Sebastian Berlin <sebastian.berlin@wikimedia.se>
2026-06-12 16:10:03 +0200

    more templates

--- src/pd_rationale_map.py
+++ src/pd_rationale_map.py
@@ -5,18 +5,34 @@ rationales = {
         "PD-old-100",
         "PD-Art-100",
         "PD-art-old-100-expired",
-        "PD-old-100-1923"
+        "PD-old-100-1923",
+        "PD-old-100-expired"
     ],
-    "Q114785307": ["PD-old-95"],
-    "Q114785302": ["PD-old-90"],
-    "Q61830521": ["PD-old-80"],
-    "Q117408984": ["PD-old-75"],
+    "Q114785307": [
+        "PD-old-95",
+        "PD-old-95-expired"
+    ],
+    "Q114785302": [
+        "PD-old-90",
+        "PD-old-90-expired"
+    ],
+    "Q61830521": [
+        "PD-old-80",
+        "PD-old-80-expired"
+    ],
+    "Q117408984": [
+        "PD-old-75",
+        "PD-old-75-expired"
+    ],
     "Q59542795": [
         "PD-old-70",
         "PD-old-70-expired",
         "PD-old-70-1923"
     ],
-    "Q59621182": ["PD-old-50"],
+    "Q59621182": [
+        "PD-old-50",
+        "PD-old-50-expired"
+    ],
     "Q60671452": [
         "PDUSgov",
         "PD-USGov-*",
